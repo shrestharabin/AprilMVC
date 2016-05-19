ef9339f11a2b4d53a1f2fc2b146e1c7b92578647
Jiwan Katuwal <jiwanje@gmail.com>
2016-05-19 07:09:58 +0545

    file added

---
+++ LFA/Shared Projects/LFA.Data/LFA.Data/Role.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LFA.Data
+{
+    public sealed class Role:Base
+    {
+        public int RoleID { get; set; }
+        public string RoleName { get; set; }
+    }
+
+    public class role1 : Role
+    { 
+    
+    }
+}
